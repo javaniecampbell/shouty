13eef7c6affcb9cecb9f0c7f00c81b6d23a48d60
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-01 13:09:33 -0500

    chore: Added console.log statements for debugging

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,15 +1,17 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
 Given('Lucy is located {int} metres from Sean', function (distance) {
-
+    console.log('distance:', distance);
     return 'pending';
 });
 
 When('Sean shouts {string}', function (message) {
+    // Write code here that turns the phrase above into concrete actions
     return 'pending';
 });
 
 
 Then('Lucy hears Sean\'s message', function () {
+    // Write code here that turns the phrase above into concrete actions
     return 'pending';
 });
