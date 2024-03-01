856c1e5d754528facfd87cf9146d0d9400ebfb8a
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-01 18:51:53 -0500

    feat: added network functionality and update message variable

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,19 +1,23 @@
-const Person = require('../../src/shouty');
+const { Network } = require('../../src/shouty');
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { assertThat, is } = require('hamjest');
 
 Given('{person} is located/standing {int} metre(s) from {person}', function (lucy, distance, sean) {
+    this.network = new Network();
     this.lucy = lucy;
     this.sean = sean;
+    this.lucy.joinNetwork(this.network);
+    this.sean.joinNetwork(this.network);
+
     this.lucy.moveTo(distance);
 });
 
 When('Sean shouts {string}', function (message) {
     this.sean.shout(message);
-    this.message = message;
+    this.messageFromSean = message;
 });
 
 
 Then('Lucy hears Sean\'s message', function () {
-    assertThat(this.lucy.messagesHeard(), is([this.message]));
+    assertThat(this.lucy.messagesHeard(), is([this.messageFromSean]));
 });
