e77eee5848b752f733ee36ac44099782f37a9ece
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-01 12:39:01 -0500

    feat: Added step definitions for Lucy and Sean interactions

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,15 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('Lucy is located {int} meters from Sean', function (distance) {
+
+    return 'pending';
+});
+
+When('Sean shouts {string}', function (message) {
+    return 'pending';
+});
+
+
+Then('Lucy hears Sean\'s message', function () {
+    return 'pending';
+});
